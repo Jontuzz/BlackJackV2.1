3e5987fce7ada4806c1dfdc18710db4516fa4da5
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-26 13:21:49 +0300

    Korttipakka

---
+++ BlackJackV2/src/blackjackv2/KorttiPakka.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjackv2;
+
+/**
+ *
+ * @author s1601378
+ */
+import java.util.ArrayList;
+
+public class KorttiPakka {
+    
+    //ArrayList korteille
+    private ArrayList<Kortti> pakka = new ArrayList<>();
+    //ArrayList korttien maille
+    private ArrayList<String> maat = new ArrayList<>();
+    
+    public KorttiPakka(int pakkojenMaara) {
+        
+    }
+}

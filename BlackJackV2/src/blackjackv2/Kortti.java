80ea2039c5263f6949c4406f5863e363c769908f
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-26 13:12:40 +0300

    Kortti luokka valmis

--- BlackJackV2/src/blackjackv2/Kortti.java
+++ BlackJackV2/src/blackjackv2/Kortti.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package blackjackv2;
 
 /**
@@ -10,5 +5,27 @@ package blackjackv2;
  * @author s1601378
  */
 public class Kortti {
+    private int arvo;
+    private String maa;
     
+    public Kortti(int arvo, String maa) {
+        this.arvo = arvo;
+        this.maa = maa;
+    }
+
+    public int getArvo() {
+        return arvo;
+    }
+
+    public void setArvo(int arvo) {
+        this.arvo = arvo;
+    }
+
+    public String getMaa() {
+        return maa;
+    }
+
+    public void setMaa(String maa) {
+        this.maa = maa;
+    }
 }

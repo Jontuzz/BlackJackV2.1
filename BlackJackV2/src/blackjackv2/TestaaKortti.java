94118afe386cdf568dd2c5dc7eedb948f8d0a500
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-26 13:18:20 +0300

    blackjack

--- BlackJackV2/src/blackjackv2/TestaaKortti.java
+++ BlackJackV2/src/blackjackv2/TestaaKortti.java
@@ -11,6 +11,9 @@ package blackjackv2;
  */
 public class TestaaKortti {
     public static void main(String[] args) {
+        Kortti testaaKortti = new Kortti(1, "Pata");
+        System.out.println(testaaKortti);
         
+        System.out.println("Kortin arvo: " + testaaKortti.getArvo() + " maa: " + testaaKortti.getMaa());
     }
 }

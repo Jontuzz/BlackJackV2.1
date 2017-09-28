ca1114c98dec5fee69a900a43983dcf48ab11d0b
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-09-28 23:13:17 +0300

    updates

--- BlackJackV2/src/blackjackv2/BlackJackV2.java
+++ BlackJackV2/src/blackjackv2/BlackJackV2.java
@@ -88,33 +88,46 @@ public class BlackJackV2 extends Application {
         tietokoneenKortitTextArea.setPrefWidth(200);
         grid.add(tietokoneenKortitTextArea, 0, 2);
 
+        
+        //Luo napin nimellä btn ja labelillä "Sign in"
+        Button lisaaKorttiButton = new Button("Lisää kortti");
+        Button lopetaButton = new Button("Lopeta");
+        
+        
+        //Luo HBox layout pane nimeltään hbBtn with spacing of 10 pixels
+        HBox hbBtn = new HBox(10);
+        
         int kortteja = 0;
         while (kortteja < 2) {
 
             Kortti jaettuKortti = pakka.jaaKortti();
             pelaajanKasi.otaKortti(jaettuKortti);
             pelaajanKortitTextArea.appendText(jaettuKortti + "\n");
+            
+            if (pelaajanKasi.selvitaSumma() < 21) {
+                lisaaKorttiButton.setVisible(true);
+            } else {
+                lisaaKorttiButton.setVisible(false);
+                lisaaKorttiButton.setDisable(true);
+                pelaajanKorttienSumma.setText("Loppusumma: " + pelaajanKasi.selvitaSumma());
+            }
+            
             kortteja++;
+            
         }
-
-        //Luo napin nimellä btn ja labelillä "Sign in"
-        Button lisaaKortti = new Button("Lisää kortti");
-        lisaaKortti.setVisible(true);
-        Button lopeta = new Button("Lopeta");
-        //Luo HBox layout pane nimeltään hbBtn with spacing of 10 pixels
-        HBox hbBtn = new HBox(10);
-
+        
+        
         /*
             The HBox pane sets an alignment for the button that is different from the alignment applied to the other controls in the grid pane.
             The alignment property has a value of Pos.BOTTOM_RIGHT, which positions a node at the bottom of the space vertically and at the right edge of the space horizontally. 
          */
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         //The button is added as a child of the HBox pane, and the HBox pane is added to the grid in column 2, row 4
-        hbBtn.getChildren().add(lopeta);
-        hbBtn.getChildren().add(lisaaKortti);
+        hbBtn.getChildren().add(lopetaButton);
+        hbBtn.getChildren().add(lisaaKorttiButton);
         grid.add(hbBtn, 2, 4);
 
-        lisaaKortti.setOnAction(new EventHandler<ActionEvent>() {
+        lisaaKorttiButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 
@@ -125,21 +138,24 @@ public class BlackJackV2 extends Application {
                 pelaajanKorttienSumma.setText("Korttien summa: " + pelaajanKasi.selvitaSumma());
                 
                 if (pelaajanKasi.selvitaSumma() < 21) {
-                    lisaaKortti.setVisible(true);
+                    lisaaKorttiButton.setVisible(true);
                 } else {
-                    lisaaKortti.setVisible(false);
-                    lisaaKortti.setDisable(true);
+                    lisaaKorttiButton.setVisible(false);
+                    lisaaKorttiButton.setDisable(true);
+                    pelaajanKorttienSumma.setText("Loppusumma: " + pelaajanKasi.selvitaSumma());
                 }
                 
                 System.out.println("Pelaajan summa: " + pelaajanKasi.selvitaSumma());
             }
         });
 
-        lopeta.setOnAction(new EventHandler<ActionEvent>() {
+        lopetaButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                lisaaKortti.setDisable(true);
-                lisaaKortti.setVisible(false);
+                lisaaKorttiButton.setDisable(true);
+                lisaaKorttiButton.setVisible(false);
+                
+                
             }
         });
         
@@ -150,6 +166,10 @@ public class BlackJackV2 extends Application {
         primaryStage.show();
     }
 
+    private void jaaTietokoneAvustajalle() {
+        while 
+    }
+    
     /**
      * @param args the command line arguments
      */
